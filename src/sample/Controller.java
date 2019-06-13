f49f8ecaacf830d3fff0db7fcb85b0d8757b1e40
Andy Ni <nakukibo@gmail.com>
2019-06-12 21:53:44 -0500

    Stable css styling

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,11 +1,13 @@
 package sample;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.input.ScrollEvent;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -16,51 +18,75 @@ import java.util.regex.Pattern;
 public class Controller {
 
     @FXML
+    private ScrollPane centerScrollPane;
+
+    //Basic Info
+    @FXML
     private TextField projectName;
     @FXML
-    private ComboBox projectType;
+    private ComboBox projectStatus;
     @FXML
     private DatePicker dateCompleted;
     @FXML
-    private GridPane skillsUsed;
-    @FXML
-    private TextField githubPage;
-    @FXML
-    private TextField googlePlay;
-    @FXML
-    private TextField learnMore;
+    private ComboBox projectType;
     @FXML
     private TextArea description;
+
+    //Skills Used
+    @FXML
+    private TilePane skillsUsed;
+
+
+    //Additional Links
+
+
+    //Submit
+    @FXML
+    private Button generate;
 
     private String htmlLocation = "C:\\Users\nakuk\\OneDrive\\Documents\\GitHub\\index.html";
 
     @FXML
+    public void initialize(){
+        dateCompleted.setVisible(false);
+        generate.setDisable(true);
+    }
+
+    @FXML
+    public void checkStatus(){
+        if(projectStatus.getValue().toString().toLowerCase().equals("published")){
+            dateCompleted.setVisible(true);
+        }else{
+            dateCompleted.setVisible(false);
+        }
+    }
+
+    @FXML
     public void onButtonClicked() {
-        String projectNameStr = projectName.getText();
-        String projectTypeStr = projectType.getValue().toString();
-        String descriptionStr = description.getText();
-        String dateCompletedStr = dateCompleted.getValue().toString();
-
-        List<Node> skillsUsedChildren = skillsUsed.getChildren();
-        List<String> skillsUsedList = new ArrayList<>();
-
-        for (Node checkBox : skillsUsedChildren) {
-            if (((CheckBox) checkBox).isSelected()) skillsUsedList.add(((CheckBox) checkBox).getText());
-        }
-
-        String githubLink = githubPage.getText();
-        String gPlayLink = googlePlay.getText();
-        String learnMoreLink = learnMore.getText();
-
-        String html = getProjectSetup(projectNameStr, projectTypeStr, descriptionStr,
-                dateCompletedStr, skillsUsedList, githubLink, gPlayLink, learnMoreLink);
-        System.out.println(html);
-        try {
-            updateHtml(html);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+//        String projectNameStr = projectName.getText();
+//        String projectTypeStr = projectType.getValue().toString();
+//        String descriptionStr = description.getText();
+//        String dateCompletedStr = dateCompleted.getValue().toString();
+//
+//        List<Node> skillsUsedChildren = skillsUsed.getChildren();
+//        List<String> skillsUsedList = new ArrayList<>();
+//
+//        for (Node checkBox : skillsUsedChildren) {
+//            if (((CheckBox) checkBox).isSelected()) skillsUsedList.add(((CheckBox) checkBox).getText());
+//        }
+//
+//        String githubLink = githubPage.getText();
+//        String gPlayLink = googlePlay.getText();
+//        String learnMoreLink = learnMore.getText();
+//
+//        String html = getProjectSetup(projectNameStr, projectTypeStr, descriptionStr,
+//                dateCompletedStr, skillsUsedList, githubLink, gPlayLink, learnMoreLink);
+//        System.out.println(html);
+//        try {
+//            updateHtml(html);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 
     private void updateHtml(String projectStr) throws IOException {
@@ -130,5 +156,10 @@ public class Controller {
         for (String skill : skills) skillBase += "<li>" + skill + "</li>";
         String skillsStr = "Skills Used:\n" + "<ul>\n" + skillBase + "\n" + "</ul>";
         return skillsStr;
+    }
+
+    private boolean formFilledOut(){
+        if(projectName.getText().trim().isEmpty()) return false;
+        return true;
     }
 }
